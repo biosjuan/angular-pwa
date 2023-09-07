4885488048ccc45a9c9d541ab012fea1c12634a3
biosjuan <hsoftwaredeveloperlee@gmail.com>
2023-09-07 16:33:34 -0700

    added PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Image, ServicesService, User } from './services/services.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-pwa';
+  user1!: User;
+  image1!: Image;
+
+  constructor(private service: ServicesService) {
+    this.user1 = { email: '', id: 0, name: '', username: '' };
+    this.image1 = { albumId: 0, thumbnailUrl: '', id: 0, title: '' };
+  }
+
+  ngOnInit(): void {
+    this.service.getPhoto().subscribe((res) => {
+      console.log(JSON.stringify(res));
+    });
+  }
+
+  getImage(): void {
+    this.service.getPhoto().subscribe((res) => {
+      this.image1 = res;
+    });
+  }
+
+  getUser(): void {
+    this.service.getUser().subscribe((res) => {
+      console.log(res);
+      this.user1 = res;
+    });
+  }
+
+  onClearImage(): void {
+    this.image1 = { albumId: 0, thumbnailUrl: '', id: 0, title: '' };
+  }
+
+  onClearUser(): void {
+    this.user1 = { email: '', id: 0, name: '', username: '' };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
